da0076cc7a559ba8d65f5412265f5a1eddb97033
RoosterRoo <desayan9@gmail.com>
2023-05-20 14:17:00 +0530

    Users blog v1.0

---
+++ forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL, Email, InputRequired
+from flask_ckeditor import CKEditorField
+
+
+# WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[InputRequired()])
+    subtitle = StringField("Subtitle", validators=[InputRequired()])
+    img_url = StringField("Blog Image URL", validators=[InputRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[InputRequired()])
+    submit = SubmitField("Submit Post")
+
+
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[Email(), InputRequired()])
+    name = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    submit = SubmitField("Register")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[Email(), InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    submit = SubmitField("Login")
